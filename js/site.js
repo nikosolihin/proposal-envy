6e8389ca036c8db5bd061b0b4a4a396321df5ac2
Niko Solihin <nikosolihin@gmail.com>
2014-08-17 17:23:15 +0700

    Add smooth scroll for anchor to div ID

--- js/site.js
+++ js/site.js
@@ -1,4 +1,18 @@
 $(function() {
+  // scroll to div ID
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 400);
+        return false;
+      }
+    }
+  });
+
   $(window).scroll(function() {
     // .css( 'opacity', -( $(this).scrollTop()/1000));
 
