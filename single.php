d4be254eb6384882169fbaad65d731ddbc689917
Niko Solihin <nikosolihin@gmail.com>
2014-08-21 14:39:55 +0700

    Finish FB OG stuff

--- single.php
+++ single.php
@@ -7,7 +7,7 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 $context['options'] = get_fields('options');
-$context['wp_title'] .= ' - ' . $post->title();
+$context['wp_title'] = 'Proposal Envy - ' . $post->title();
 $context['post_type'] = ucwords($post_type);
 
 Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig'), $context);
